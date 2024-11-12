36cce34878089f875df52c403f077a3379638a5b
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-11-12 17:35:28 +0100

    formulaire Editor ok avec controller et interface twig

--- src/Controller/Admin/EditorController.php
+++ src/Controller/Admin/EditorController.php
@@ -2,36 +2,36 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Author;
-use App\Form\AuthorType;
+use App\Entity\Editor;
+use App\Form\EditorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/author')]
-class AuthorController extends AbstractController
+#[Route('/admin/editor')]
+class EditorController extends AbstractController
 {
-    #[Route('', name: 'app_admin_author', methods: ['GET'])]
+    #[Route('', name: 'app_admin_editor', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('admin/author/index.html.twig', [
-            'controller_name' => 'AuthorController',
+        return $this->render('admin/editor/index.html.twig', [
+            'controller_name' => 'EditorController',
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_editor_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
-        $author = new Author();
-        $form = $this->createForm(AuthorType::class, $author);
+        $editor = new Editor();
+        $form = $this->createForm(EditorType::class, $editor);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             // faire quelque chose
         }
 
-        return $this->render('admin/author/new.html.twig', [
+        return $this->render('admin/editor/new.html.twig', [
             'form' => $form,
             ]);
     }
